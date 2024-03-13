1c9396b1fa35fdc3c2d981791766b649b61e53fc
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-03-13 17:45:56 +0000

    Deploying to gh-pages from @ medeea98/group-project-js-5@5491509b400635ae0f739ffb0f1ef87c09f29413 🚀

--- index.821089d8.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("btn-watched").onmouseover=function(){document.getElementById("btn-watched").style.backgroundColor="red"},document.getElementById("btn-watched").onmouseout=function(){document.getElementById("btn-watched").style.backgroundColor="blanchedalmond"};
-//# sourceMappingURL=index.821089d8.js.map
--- index.a84ea87b.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("btn-watched").onmouseover=function(){document.getElementById("btn-watched").style.backgroundColor="red"},document.getElementById("btn-watched").onmouseout=function(){document.getElementById("btn-watched").style.backgroundColor="blanchedalmond"};
-//# sourceMappingURL=index.a84ea87b.js.map
