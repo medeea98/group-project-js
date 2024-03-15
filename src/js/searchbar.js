843cb94a3c103db1bc06ef2f3c8df50bbb324181
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-03-15 13:05:57 +0200

    searchbar ready

--- src/js/searchbar.js
+++ src/js/searchbar.js
@@ -2,90 +2,160 @@
 import { apiKey, apiUrlBase } from './api-key.js';
 
 async function fetchData(url, options) {
-    try {
-      const response = await fetch(url, options);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return null;
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
   }
-  
-  async function searchMoviesByTitle(title) {
-    const apiUrl = `${apiUrlBase}/search/movie?api_key=${apiKey}&query=${title}&language=${language}&include_adult=false&page=1`;
-  
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${accessToken}`
-      }
-    };
-  
-    return await fetchData(apiUrl, options);
+}
+
+async function getMovies(endpoint) {
+  const apiUrl = `${apiUrlBase}/${endpoint}?api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error at ${apiUrl}`, error);
   }
+}
+
+async function getGenres() {
+  return getMovies('/genre/movie/list');
+}
+
+async function getPopularMovies() {
+  return getMovies('/trending/movie/week');
+}
   
-  async function searchAndDisplayMovies(title) {
-    const movies = await searchMoviesByTitle(title);
-  
-    const moviesContainer = document.getElementById('movie-container');
-    moviesContainer.innerHTML = '';
-  
-    if (movies.length === 0) {
-      const errorMessage = document.createElement('p');
-      errorMessage.textContent = 'Nu s-au găsit filme pentru căutarea dată.';
-      moviesContainer.appendChild(errorMessage);
-    } else {
-      movies.forEach(movie => {
-        const movieCard = createMovieCard(movie);
-        moviesContainer.appendChild(movieCard);
-      });
+async function searchMovies(title) {
+  const apiUrl = `${apiUrlBase}/search/movie?api_key=${apiKey}&query=${title}&language=en-US&include_adult=false&page=1`;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
     }
-  }
-  function createMovieCard(movie) {
-    const movieCard = document.createElement('div');
-    movieCard.classList.add('movie-card');
-  
-    const movieImage = document.createElement('img');
-    movieImage.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-    movieImage.alt = `${movie.title} Poster`;
-    movieImage.classList.add('movie-image');
-  
-    const movieDescription = document.createElement('div');
-    movieDescription.classList.add('movie-description');
-  
-    const movieTitle = document.createElement('h2');
-    movieTitle.textContent = movie.title;
-    movieTitle.classList.add('movie-title');
-  
-    const movieGenreYearRating = document.createElement('p');
-    movieGenreYearRating.classList.add('movie-details');
-    movieGenreYearRating.textContent = `Release Date: ${movie.release_date}, Rating: ${movie.vote_average}`;
-  
-    movieDescription.appendChild(movieTitle);
-    movieDescription.appendChild(movieGenreYearRating);
-  
-    movieCard.appendChild(movieImage);
-    movieCard.appendChild(movieDescription);
-  
-    return movieCard;
-  }
-  
-  window.onload = () => {
-    const searchInput = document.getElementById('search-input');
-    const searchForm = document.getElementById('search-bar');
-  
-    searchForm.addEventListener('submit', async event => {
-      event.preventDefault();
-      const searchQuery = searchInput.value.trim();
-      if (searchQuery !== '') {
-        await searchAndDisplayMovies(searchQuery);
-      }
-    });
   };
 
+  return await fetchData(apiUrl, options);
+}
 
+function roundRating(rating) {
+  const roundedRating =
+    Math.abs(rating % 1) >= 0.005
+      ? Math.ceil(rating * 100) / 100
+      : Math.floor(rating * 100) / 100;
+  return roundedRating;
+}
+  
+async function searchAndDisplayMovies(title) {
+  let movies;
+  if (title) {
+    movies = await searchMovies(title);
+  } else {
+    movies = await getPopularMovies();
+  }
+
+  const moviesContainer = document.getElementById('movie-container');
+  moviesContainer.innerHTML = '';
+
+  if (movies.results.length === 0) {
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = 'Nu s-au găsit filme pentru căutarea dată.';
+    moviesContainer.appendChild(errorMessage);
+    document.querySelector('.movies').hidden = true;
+  } else {
+    document.querySelector('.movies').remove();
+    const moviesContainer = document.createElement('div');
+    moviesContainer.classList.add('movies');
+  
+    const movieContainer = document.createElement('div');
+    movieContainer.id = 'movie-container';
+  
+    moviesContainer.appendChild(movieContainer);
+  
+    document.body.appendChild(moviesContainer);
+
+    const genres = await getGenres();
+    
+    movies.results.forEach(movie => {
+      createMovieCard(movie, genres, movieContainer);
+    });
+  }
+}
+
+function createMovieCard(movie, genres, movieContainer) {
+  const movieCard = document.createElement('div');
+  movieCard.classList.add('movie-card');
+
+  const movieImage = document.createElement('img');
+  movieImage.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  movieImage.alt = `${movie.title} Poster`;
+  movieImage.classList.add('movie-image');
+
+  const movieDescription = document.createElement('div');
+  movieDescription.classList.add('movie-description');
+
+  const movieTitle = document.createElement('h2');
+  movieTitle.textContent = movie.title;
+  movieTitle.classList.add('movie-title');
+
+  const movieGenres = movie.genre_ids.map(genreId => {
+    const genre = genres.genres.find(g => g.id === genreId);
+    return genre ? genre.name : '';
+  });
+
+  const displayedGenres = movieGenres.slice(0, 2);
+  const otherGenres = movieGenres.slice(2);
+
+  const roundedRating = roundRating(movie.vote_average);
+
+  const movieGenreYearRating = document.createElement('p');
+
+  const ratingSpan = document.createElement('span');
+  ratingSpan.classList.add('rating');
+  ratingSpan.textContent = ` ${roundedRating}`;
+
+  if (otherGenres.length > 0) {
+    movieGenreYearRating.innerHTML = `${displayedGenres.join(
+      ', '
+    )}, Other | ${
+      movie.release_date ? movie.release_date.substring(0, 4) : 'N/A'
+    } | `;
+  } else {
+    movieGenreYearRating.innerHTML = `${displayedGenres.join(', ')} | ${
+      movie.release_date ? movie.release_date.substring(0, 4) : 'N/A'
+    } | `;
+  }
+
+  movieGenreYearRating.appendChild(ratingSpan);
+
+  movieGenreYearRating.classList.add('movie-details');
+
+  movieDescription.appendChild(movieTitle);
+  movieDescription.appendChild(movieGenreYearRating);
+
+  movieCard.appendChild(movieImage);
+  movieCard.appendChild(movieDescription);
+
+  movieContainer.appendChild(movieCard);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const searchInput = document.querySelector('.search-input');
+  const searchForm = document.querySelector('.search-bar');
+
+  searchForm.addEventListener('submit', async event => {
+    event.preventDefault();
+    const searchQuery = searchInput.value.trim();
+    await searchAndDisplayMovies(searchQuery);
+  });
+})
